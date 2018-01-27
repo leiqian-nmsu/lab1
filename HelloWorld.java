2658a136e0c09fe5b084d0bdc3ff09d0f7745bd0
Lei Qian <leiqian@nmsu.edu>
2018-01-26 21:18:33 -0700

    This is a HelloWorld.java file.

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+// CS371
+// Program Name: lab1
+// Author: Lei Qian
+// Date: 01/26/2018
+// Purpose: initial Git
+
+import java.util.*;
+
+public class HelloWorld {
+
+   public static void main(String args[]) {
+   
+      System.out.println("Hello World!");
+       
+   } // end main
+   
+} // end HelloWorld class
+      
